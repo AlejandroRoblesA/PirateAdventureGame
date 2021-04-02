5d918a0cc0d70a9598ee4bd4ec27a14beb526795
AlejandroRoblesA <alejandro_r1704@hotmail.com>
2021-04-01 19:49:30 -0600

    IBOutles and IBActions

--- Pirate Adventure Game/ViewController.m
+++ Pirate Adventure Game/ViewController.m
@@ -9,6 +9,19 @@
 
 @interface ViewController ()
 
+@property (weak, nonatomic) IBOutlet UILabel     *healthLabel;
+@property (weak, nonatomic) IBOutlet UILabel     *damageLabel;
+@property (weak, nonatomic) IBOutlet UILabel     *weaponLabel;
+@property (weak, nonatomic) IBOutlet UILabel     *armorLabel;
+@property (weak, nonatomic) IBOutlet UILabel     *storyLabel;
+@property (weak, nonatomic) IBOutlet UIButton    *actionButton;
+@property (weak, nonatomic) IBOutlet UIImageView *itemImageView;
+@property (weak, nonatomic) IBOutlet UIButton    *northButton;
+@property (weak, nonatomic) IBOutlet UIButton    *eastButton;
+@property (weak, nonatomic) IBOutlet UIButton    *southButton;
+@property (weak, nonatomic) IBOutlet UIButton    *westButton;
+
+
 @end
 
 @implementation ViewController
@@ -18,5 +31,17 @@
     // Do any additional setup after loading the view.
 }
 
+- (IBAction)actionButtonPressed:(UIButton *)sender {
+}
+- (IBAction)northButtonPressed:(UIButton *)sender {
+}
+- (IBAction)eastButtonPressed:(UIButton *)sender {
+}
+- (IBAction)southButtonPressed:(UIButton *)sender {
+}
+- (IBAction)westButtonPressed:(id)sender {
+}
+- (IBAction)resetButtonPressed:(UIButton *)sender {
+}
 
 @end
